ebf818e271dc8851c099c7d842d89464cc19c02c
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-20 15:07:54 +0900

    2023-11-20

---
+++ src/2023-11-20/solution.ts
@@ -0,0 +1,12 @@
+export const solutions = {
+  // https://school.programmers.co.kr/learn/courses/30/lessons/120804
+  1: (num1, num2) => num1 * num2,
+  // https://school.programmers.co.kr/learn/courses/30/lessons/120807
+  2: (num1, num2) => num1 === num2 ? 1 : -1,
+  // https://school.programmers.co.kr/learn/courses/30/lessons/120805
+  3: (num1: number, num2: number) => parseInt(String(num1 / num2), 10),
+  // https://school.programmers.co.kr/learn/courses/30/lessons/120803
+  4: (num1, num2) => num1 - num2,
+  // https://school.programmers.co.kr/learn/courses/30/lessons/120820
+  5: (age: number) => 2022 - age + 1,
+}
